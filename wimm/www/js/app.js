2719a6857dde8e9dacb2b4729786896414508073
Eugene Kileiko <e.kileiko@gmail.com>
2016-06-24 00:05:56 +0300

    Fixed start page to blank

--- wimm/www/js/app.js
+++ wimm/www/js/app.js
@@ -113,5 +113,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'ng
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/categories');
+  $urlRouterProvider.otherwise('/app/blank');
 });
