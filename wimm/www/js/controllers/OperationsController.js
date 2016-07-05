7cabf37913ac14320a21af34cb1b937a6885dd9a
Eugene Kileiko <e.kileiko@gmail.com>
2016-07-05 20:17:47 +0300

    Test material animations

--- wimm/www/js/controllers/OperationsController.js
+++ wimm/www/js/controllers/OperationsController.js
@@ -1,7 +1,12 @@
-app.controller('OperationsController', function($scope, OperationsService) {
+app.controller('OperationsController', function($scope, OperationsService, ionicMaterialInk, ionicMaterialMotion, $timeout) {
     console.log('OperationsController...');
 
     $scope.operations = OperationsService.getOperations();
+    
+    $timeout(function(){
+        ionicMaterialInk.displayEffect();
+        ionicMaterialMotion.ripple();
+    },100);
 })
 .controller('OperationController', function($scope, $stateParams, OperationsService) {
     console.log('OperationController...');
